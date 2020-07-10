d6399d54ce5ca4f9607c6897987e00ae080eae7d
kollieartwolf <titoffklim@cclc.tech>
2020-07-11 00:18:03 +0300

    Update Figures.cs

--- Figures.cs
+++ Figures.cs
@@ -3,6 +3,11 @@ using System;
 
 namespace FiguresTest
 {
+    /*!
+     * @class Figures
+     * @brief Статичный класс, который реализует единственный метод - вычисление
+     * площади фигуры.
+     */
     public static class Figures
     {
         public static double GetSquare(ISquarable squarable)
@@ -10,86 +15,134 @@ namespace FiguresTest
             return squarable.CountSquare();
         }
     }
+    /*!
+     * @interface ISquarable
+     * @brief Интерфейс, задающий метод вычисления площади.
+     */
     public interface ISquarable
     {
         double CountSquare();
     }
-
+    /*!
+     * @class Point
+     * @brief Класс точки на плоскости.
+     * @details Содержит два поля - координаты точки в декартовой СК.
+     */
     public class Point
     {
-        public double _x { get; set; }
-        public double _y { get; set; }
-        public Point(double x, double y)
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Point(double _x, double _y)
         {
-            _x = x;
-            _y = y;
+            X = _x;
+            Y = _y;
+        }
+        /*!
+         * @fn Len
+         * @brief Функция для вычисления длины от текущей точки до другой.
+         * @param p2 точка, до которой мы будем вычислять длину
+         * @returns длину между точками
+         */
+        public double Len(Point p2)
+        {
+            return Math.Pow((X - p2.X) * (X - p2.X) + (Y - p2.Y) * (Y - p2.Y),
+                0.5);
         }
     }
-
+    /*!
+     * @class Pointed
+     * @brief Класс n-угольника.
+     * @details Содержит в себе список точек многоугольника, а также реализует
+     * метод Гаусса для вычисления площади многоугольника и метод вычисления
+     * длины между двумя точками.
+     */
     public class Pointed : ISquarable
     {
-        protected List<Point> _pointList { get; set; }
-        public Pointed(List<Point> pointList)
+        protected List<Point> PointList { get; set; }
+        public Pointed(List<Point> _pointList)
         {
-            _pointList = pointList;
+            PointList = _pointList;
         }
-        public void AddPoint(Point point)
-        {
-            _pointList.Add(point);
-        }
+        /*!
+         * @fn CountSquare
+         * @brief Метод вычисления площади n-угольника.
+         * @details Используется формула площади Гаусса для n-угольника. При
+         * условии, что точки упорядочены в порядке обхода, даёт правильный
+         * результат.
+         * @example {@code Pointed rect = new Pointed(new List<Point>{new
+         * Point(0, 0), new Point(2, 0), new Point(2, 4), new Point(0, 4)}); }
+         * Вышеуказанный код создаёт прямоугольник, причём вершины задаются в
+         * порядке обхода.
+         * @bug Если задать вершины в ином порядке, метод выдаст нулевую площадь
+         * по 2-м причинам:
+         * 1) он посчитает, что задан не прямоугольник, а два треугольника,
+         * соприкасающихся в центре пересечения диагоналей;
+         * 2) он вычтет их площади друг из друга, подобно тому как интеграл не
+         * считает площадь под графиком, если график находится ниже прямой y=0.
+         * @returns площадь многоугольника
+         */
         public double CountSquare()
         {
-            /*! Формула площади Гаусса для n-угольника. */
             double A = 0.0;
-            for (int i = 0; i < _pointList.Count - 1; i++)
+            for (int i = 0; i < PointList.Count - 1; i++)
             {
-                A += _pointList[i]._x * _pointList[i + 1]._y;
+                A += PointList[i].X * PointList[i + 1].Y;
             }
-            A += _pointList[_pointList.Count - 1]._x * _pointList[0]._y;
-            for (int i = 0; i < _pointList.Count - 1; i++)
+            A += PointList[PointList.Count - 1].X * PointList[0].Y;
+            for (int i = 0; i < PointList.Count - 1; i++)
             {
-                A -= _pointList[i + 1]._x * _pointList[i]._y;
+                A -= PointList[i + 1].X * PointList[i].Y;
             }
-            A -= _pointList[0]._x * _pointList[_pointList.Count - 1]._y;
+            A -= PointList[0].X * PointList[PointList.Count - 1].Y;
             return Math.Abs(A) / 2;
         }
-        public double Len(Point p1, Point p2)
-        {
-            return Math.Pow((p1._x - p2._x) * (p1._x - p2._x) + (p1._y - p2._y)
-                * (p1._y - p2._y), 0.5);
-        }
     }
-
+    /*!
+     * @class Triangle
+     * @brief Частный класс Pointed - класс треугольников.
+     * @details Задаётся тремя точками и имеет метод определения, правильный ли
+     * это треугольник или нет.
+     */
     public class Triangle : Pointed
     {
         public Triangle(Point p1, Point p2, Point p3) : base(new List<Point>
             { p1, p2, p3 })
         {
         }
+        /*!
+         * @fn IsRightTriangle
+         * @brief Определяет при помощи теоремы Пифагора, является ли
+         * треугольник правильным.
+         * @returns булево значение, правильный ли треугольник
+         * @retval true треугольник правильный
+         * @retval false треугольник остроугольный/тупоугольный
+         */
         public bool IsRightTriangle()
         {
-            List<double> sides = new List<double>{ Len(_pointList[0],
-                _pointList[1]),
-                Len(_pointList[0], _pointList[2]),
-                Len(_pointList[1], _pointList[2]) };
+            List<double> sides = new List<double>{ PointList[0].Len(
+                PointList[1]), PointList[0].Len(PointList[2]),
+                PointList[1].Len(PointList[2]) };
             sides.Sort();
-            /*! С учётом точности вычисления Math.Pow зададим eps = 0.001 
+            /*! С учётом точности вычисления Math.Pow зададим eps = 0.001
              * для сравнения. */
             return Math.Abs(Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) -
                 Math.Pow(sides[2], 2)) < 0.001;
         }
     }
-
+    /*!
+     * @class Circle
+     * @brief Класс круга. Содержит поле радиуса и метод вычисления площади.
+     */
     public class Circle : ISquarable
     {
-        private double _R { get; set; }
-        public Circle(double R)
+        private double R { get; set; }
+        public Circle(double _R)
         {
-            _R = R;
+            R = _R;
         }
         public double CountSquare()
         {
-            return Math.PI * _R * _R;
+            return Math.PI * R * R;
         }
     }
 }
